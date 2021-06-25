d9ded7f3f21e5af31e93d78935560570b2b438cc
Rakesh Rajput <50516603+Rakeshrajput999@users.noreply.github.com>
2021-06-25 19:44:57 +0530

    Create ans4.js

---
+++ day6/ans4.js
@@ -0,0 +1,9 @@
+ myColor = ["Red", "Green", "White", "Black"];
+ function joinit(a)
+ {
+     console.log(a.join(","));
+     console.log(a.join("."))
+     console.log(a.join("+"))
+ }
+
+ joinit(myColor);
