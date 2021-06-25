a8263b0d5c6c8524b0cff444657fe40e91ac7522
Rakesh Rajput <50516603+Rakeshrajput999@users.noreply.github.com>
2021-06-25 19:44:20 +0530

    Create ans3.js

---
+++ day6/ans3.js
@@ -0,0 +1,10 @@
+function first(a,n=0){
+    
+    x=a.slice(0,n)  ;
+    return x ;
+}
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
