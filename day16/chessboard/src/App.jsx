6b414d67a20e2f33cd99a0b8832d6d8aef0c5303
Rakeshrajput999 <baajigarrakesh@gmail.com>
2021-07-07 13:39:05 +0530

    day 16 chessboard

---
+++ day16/chessboard/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Chessbox from "./components/chessbox"
+
+function App() {
+  return (
+    <div className="App">
+       <Chessbox/>
+       <Chessbox/>
+       <Chessbox/>
+       <Chessbox/>
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ day16/chessboard/src/components/chessbox.jsx
@@ -0,0 +1,30 @@
+const Chessbox=()=>{
+    return (
+        
+        <div className ="chessbox">
+            <div className="odd">
+            <span >a</span>
+            <span>b</span>
+            <span>c</span>
+            <span>d</span>
+            <span>e</span>
+            <span>f</span>
+            <span>g</span>
+            <span>h</span>
+            </div>
+            <div className="even">
+            <span >a</span>
+            <span>b</span>
+            <span>c</span>
+            <span>d</span>
+            <span>e</span>
+            <span>f</span>
+            <span>g</span>
+            <span>h</span>
+            </div>
+
+        </div>
+        
+    )
+}
+export default Chessbox
