0e2486d9723053e8b75c35c1899cc9032deb0b0e
Rakeshrajput999 <baajigarrakesh@gmail.com>
2021-07-22 10:22:23 +0530

    updated

--- day20/food-calories/src/App.js
+++ day20/food-calories/src/App.js
@@ -1,12 +1,19 @@
 
+import { useState } from 'react';
 import './App.css';
-import ScrollBar from './components/ScrollBar';
-
+import Card from './components/Card';
+const arr=[
+  {id:0,title:"poha",calories:34}
+]
 function App() {
+  const [food, setFood] = useState("arr")
   return (
     <div className="App">
       <header className="App-header">
-        <ScrollBar/>
+       { food.length ==0 ?<h1>Please eat some healthy food</h1>:
+        food.map((item,index)=><card key={index} item={item} setFood={setFood}/>
+        )} 
+
       </header>
     </div>
   );
--- day20/food-calories/src/components/Card.js
+++ day20/food-calories/src/components/Card.js
@@ -1,11 +1,19 @@
+import { useState } from "react"
+
 const Card=(probs)=>{
-    let {food ,calory}=probs
+    let {item,food,setFood}=probs
+    const handleClick=()=>
+{
+    let NewState =food.filter((el)=>el.id!==item.id)
+    setFood(NewState)
+}
    return(
        <div className="Card">
            <section className="card_sec">
-               <h1>{food}</h1>
+               <h1>{item.title}</h1>
                
-               <p>you have consumed {calory} cals today</p>
+               <p>you have consumed {item.calory} cals today</p>
+            <button onClick={handleClick}></button>
            </section>
        </div>
    )
